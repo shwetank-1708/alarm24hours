3ed68204eedebe647dcdabe36a075ad06d3b3d7d
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-12-27 20:28:32 +0530

    Added Scrolldown Menu

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [visible, setVisible] = useState<boolean>(false);
 
   return (
-    <div className="flex lg:justify-around justify-between shadow-xl p-4 items-center sticky top-0 bg-white">
+    <div className="flex lg:justify-around justify-between shadow-xl p-4 items-center sticky top-0 bg-white text-sm">
       <div>
         <img src={logo} className="w-[200px]" />
       </div>
@@ -20,17 +20,71 @@ const Navbar = () => {
         <NavLink to="/about" className="hover:text-sky-400">
           <p>ABOUT US</p>
         </NavLink>
-        <NavLink to="/residential" className="hover:text-sky-400">
-          <p>RESIDENTIAL</p>
+        <NavLink to="/residential" className="group relative">
+          <div>
+            <p className="hover:text-sky-400">RESIDENTIAL</p>
+          </div>
+
+          <div className="group-hover:block hidden absolute dropdown-menu bg-white w-[250px] left-[-15px] top-5 p-4 shadow-2xl">
+            <div className="flex flex-col gap-2">
+              <p className="hover:text-sky-400">HOME SECURITY</p>
+              <hr />
+              <p className="hover:text-sky-400">VIDEO MONITORING</p>
+              <hr />
+              <p className="hover:text-sky-400">ENERGY MANAGEMENT</p>
+              <hr />
+              <p className="hover:text-sky-400">ACCESS CONTROL</p>
+              <hr />
+              <p className="hover:text-sky-400">APPS</p>
+            </div>
+          </div>
         </NavLink>
-        <NavLink to="/business" className="hover:text-sky-400">
-          <p>BUSINESS</p>
+        <NavLink to="/business" className="group relative">
+          <div>
+            <p className="hover:text-sky-400">BUSINESS</p>
+          </div>
+
+          <div className="group-hover:block hidden absolute dropdown-menu bg-white w-[250px] left-[-15px] top-5 p-4 shadow-2xl">
+            <div className="flex flex-col gap-2">
+              <p className="hover:text-sky-400">BUSINESS SECURITY ALARM</p>
+              <hr />
+              <p className="hover:text-sky-400">SURVEILLANCE SOLUTIONS</p>
+              <hr />
+              <p className="hover:text-sky-400">BUSINESS ACCESS CONTROL</p>
+            </div>
+          </div>
         </NavLink>
-        <NavLink to="/products" className="hover:text-sky-400">
-          <p>PRODUCTS</p>
+        <NavLink to="/products" className="group relative">
+          <div>
+            <p className="hover:text-sky-400">PRODUCTS</p>
+          </div>
+
+          <div className="group-hover:block hidden absolute dropdown-menu bg-white w-[250px] left-[-15px] top-5 p-4 shadow-2xl">
+            <div className="flex flex-col gap-2">
+              <p className="hover:text-sky-400">QOLSYS IQ PANEL 4</p>
+              <hr />
+              <p className="hover:text-sky-400">HIKVISION</p>
+              <hr />
+              <p className="hover:text-sky-400">ALARM.COM</p>
+              <hr />
+              <p className="hover:text-sky-400">HONEYWELL</p>
+            </div>
+          </div>
         </NavLink>
-        <NavLink to="/resources" className="hover:text-sky-400">
-          <p>RESOURCES</p>
+        <NavLink to="/resources" className="group relative">
+          <div>
+            <p className="hover:text-sky-400">RESOURCES</p>
+          </div>
+
+          <div className="group-hover:block hidden absolute dropdown-menu bg-white w-[250px] left-[-15px] top-5 p-4 shadow-2xl">
+            <div className="flex flex-col gap-2">
+              <p className="hover:text-sky-400">CONTACT</p>
+              <hr />
+              <p className="hover:text-sky-400">BLOG</p>
+              <hr />
+              <p className="hover:text-sky-400">CAREERS</p>
+            </div>
+          </div>
         </NavLink>
       </div>
 
